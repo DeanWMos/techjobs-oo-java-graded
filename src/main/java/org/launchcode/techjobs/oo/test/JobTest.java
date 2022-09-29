c5da8ebe822df241de216d4ec35bbeef699b1110
deanw <deanwademosley@gmail.com>
2022-09-29 13:21:47 -0500

    Added assert true tests

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,9 +3,9 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.*;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -19,6 +19,23 @@ public class JobTest {
         Job job2 = new Job();
 
         assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job testAll = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+//        Test if true
+        assertTrue(testAll.getName() instanceof String);
+        assertTrue(testAll.getEmployer() instanceof Employer);
+        assertTrue(testAll.getLocation() instanceof Location);
+        assertTrue(testAll.getPositionType() instanceof PositionType);
+        assertTrue(testAll.getCoreCompetency() instanceof CoreCompetency);
+
+        //Test if equal
+        assertEquals(testAll.getName() = String);
+
+
     }
 
 }
