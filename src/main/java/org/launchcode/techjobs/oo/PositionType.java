5fd7abd344e802de56a59b75f118aaf50fc537bb
deanw <deanwademosley@gmail.com>
2022-09-30 00:30:19 -0500

    finished DRY

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -2,27 +2,17 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class PositionType {
+public class PositionType extends JobField {
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
 
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
-
-    public PositionType(String value) {
-        this();
-        this.value = value;
-    }
+    public PositionType (String value) {
+        super(value);}
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
     @Override
     public String toString() {
-        return value;
+        return getValue();
     }
 
 
@@ -36,24 +26,24 @@ public class PositionType {
         return getId() == positionType.getId();
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
 
 
     // Getters and Setters:
 
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 
 }
